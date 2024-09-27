6e7b18248f369acf3ccbda530021d9fd8b079c45
feraska <feras.94@hotmail.com>
2024-09-28 00:57:57 +0300

    fix scroll & redux state managemnet

--- frontend/components/cards/Cards.tsx
+++ frontend/components/cards/Cards.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react"
 import useApi from "../../hooks/useApi"
-import { FlatList, Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { FlatList, StyleSheet, View } from "react-native"
 import CardItem from "../cardItem/CardItem"
-import { responsiveHeight, responsiveWidth } from "react-native-responsive-dimensions"
-import { useNavigation } from "@react-navigation/native"
 const Cards:React.FC<{url:string,genre?:boolean}> = ({url,genre}) => {
     const {data,getData} = useApi(url)
     useEffect(()=> {
@@ -13,31 +11,10 @@ const Cards:React.FC<{url:string,genre?:boolean}> = ({url,genre}) => {
         get()
 
     },[])
-    // const navigation =useNavigation()
-
-  //   const handleOnClick=(movieData)=>{
-  //  navigation.navigate('VideoPlayer',{movieData})
-  //   }
-    // const renderMovieCards = ({item, index}) => {
-    //   return (
-    //     <TouchableOpacity onPress={()=>handleOnClick(item)} >
-    //       <Image
-    //         resizeMode="center"
-    //         style={styles.movieImg}
-    //         source={{
-    //           uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}`,
-    //         }}
-    //       />
-    //     </TouchableOpacity>
-    //   );
-    // };
+   
     return(
         < View style={styles.view}>
-          {/* <ScrollView horizontal showsVerticalScrollIndicator={false} >
-          {data?.results.map((item,i)=>(
-            <CardItem key={i} item={item}/>
-          ))}
-          </ScrollView> */}
+        
         <FlatList
           horizontal
           ItemSeparatorComponent={() => <View style={{width: 15}}></View>}
@@ -57,17 +34,6 @@ const styles = StyleSheet.create({
       flexDirection:"row",
       gap:15
     },
-    // container: {
-    //     height: responsiveHeight(40),
-    //     gap: 15,
-    //     marginTop: 10,
-    //   },
-    //   movieImg: {
-    //     width: responsiveWidth(50),
-    //     height: '100%',
-    //     borderRadius: 10,
-    //   },
-    
    
   });
 export default Cards
